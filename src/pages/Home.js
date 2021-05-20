0c991691898612f08a491af9167f6715a86527c3
gedunlap <g.e.dunlap90@gmail.com>
2021-05-20 17:08:44 -0400

    styling and skeleton work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
 function Home(props) {
-    return <h1>Home</h1>
+    return (
+        <div className="container">
+            <div className="row iconsRow">
+                <p className="one-third column routeIcon" id="run">Run</p>
+                <p className="one-third column routeIcon" id="hike">Hike</p>
+                <p className="one-third column routeIcon" id="scenic">Scenic Walk</p>
+                <p className="twelve columns description">Welcome to "insert name here"! A collaborative effort bewteen people in motion, to share their favorite routes around the world.</p>
+            </div>
+        </div>
+    )
 }
 
 export default Home
